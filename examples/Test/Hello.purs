7054e18d13010075853091670ba57ef98b5ca0e4
Anupam Jain <ajnsit@gmail.com>
2018-06-12 11:37:47 +0530

    Use the new DomProps methods in the Hello example

--- examples/Test/Hello.purs
+++ examples/Test/Hello.purs
@@ -4,13 +4,16 @@ import Prelude
 
 import Concur.Core (Widget)
 import Concur.React (HTML)
-import Concur.React.Widgets (textButton')
+import Concur.React.DomProps (input)
+import Concur.React.Props (_type, defaultValue, onClick)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (log)
 
 helloWidget :: forall a. Widget HTML a
 helloWidget = do
-  textButton' "Click to Say Hello"
+  button "Click to Say Hello"
   liftEff (log "You said Hello!")
-  textButton' "Hello Sailor!"
+  button "Hello Sailor!"
   helloWidget
+  where
+    button s = input [_type "button", defaultValue s, unit <$ onClick] []
