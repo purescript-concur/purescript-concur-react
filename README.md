896fb2b8a859feb39ebc07d51e637860a76ebfa3
Anupam Jain <ajnsit@gmail.com>
2018-07-12 12:46:48 +0530

    Extend Todo example.

--- README.md
+++ README.md
@@ -55,8 +55,9 @@ Individual example sources -
 1. Hello World! Shows simple effectful widgets. [Source](https://github.com/ajnsit/purescript-concur/blob/master/examples/Test/Hello.purs).
 2. A simple counter widget. [Source](https://github.com/ajnsit/purescript-concur/blob/master/examples/Test/Counter.purs).
 3. Concur has Signals! Example counting widget implemented with Signals! [Source](https://github.com/ajnsit/purescript-concur/blob/master/examples/Test/Signals.purs).
-4. A postfix calculator. [Source](https://github.com/ajnsit/purescript-concur/blob/master/examples/Test/Calc.purs).
-5. Using AJAX and handling JSON responses. [Source](https://github.com/ajnsit/purescript-concur/blob/master/examples/Test/Ajax.purs).
-6. A small widget to visualise CSS color codes. [Source](https://github.com/ajnsit/purescript-concur/blob/master/examples/Test/Color.purs).
-7. Asynchronous timers which can be cancelled. [Source](https://github.com/ajnsit/purescript-concur/blob/master/examples/Test/Timers.purs).
-8. Performance test - A huge list of 50 thousand parallel buttons. This has two variants, fast (uses slightly lower level interface) and slow (idiomatic concur code). [Source](https://github.com/ajnsit/purescript-concur/blob/master/examples/Test/SlowButtonList.purs).
+4. A full-featured TodoMVC implementation with Signals. [Source](https://github.com/ajnsit/purescript-concur/blob/master/examples/Test/Todos.purs).
+5. A postfix calculator. [Source](https://github.com/ajnsit/purescript-concur/blob/master/examples/Test/Calc.purs).
+6. Using AJAX and handling JSON responses. [Source](https://github.com/ajnsit/purescript-concur/blob/master/examples/Test/Ajax.purs).
+7. A small widget to visualise CSS color codes. [Source](https://github.com/ajnsit/purescript-concur/blob/master/examples/Test/Color.purs).
+8. Asynchronous timers which can be cancelled. [Source](https://github.com/ajnsit/purescript-concur/blob/master/examples/Test/Timers.purs).
+9. Performance test - A huge list of 50 thousand parallel buttons. This has two variants, fast (uses slightly lower level interface) and slow (idiomatic concur code). [Source](https://github.com/ajnsit/purescript-concur/blob/master/examples/Test/SlowButtonList.purs).
--- examples/Test/Todos.purs
+++ examples/Test/Todos.purs
@@ -3,7 +3,7 @@ module Test.Todos where
 import Prelude
 
 import Concur.Core (Widget)
-import Concur.Core.FRP (Signal, always, hold, loopS, loopW, oneShot)
+import Concur.Core.FRP (Signal, always, dyn, hold, loopS, loopW)
 import Concur.React (HTML)
 import Concur.React.DOM as D
 import Concur.React.Props as P
@@ -14,26 +14,55 @@ import Data.Maybe (Maybe(..))
 import Data.Traversable (traverse)
 
 -- A proof of concept, Mini TodoMVC with Signals!
--- Supports Todo creation, editing, and deletion.
+-- Supports Todo creation, completion, editing, and deletion.
+
+data Filter = All | Active | Completed
+derive instance eqFilter :: Eq Filter
+instance showFilter :: Show Filter where
+  show All = "All"
+  show Active = "Active"
+  show Completed = "Completed"
+
+type Todo = {name :: String, done :: Boolean}
+type Todos = {filter :: Filter, todos :: Array Todo}
 
 todosWidget :: forall a. Widget HTML a
-todosWidget = do
-  ts <- oneShot $ todos []
-  D.div'[ D.div' [D.text ("Your list: " <> show ts)]
-        , todosWidget
-        ]
+todosWidget = dyn $ todos {filter: All, todos: []}
 
-mkTodo :: Array String -> Signal HTML (Array String)
-mkTodo ts = loopW ts \ts' -> flip cons ts' <$> textInputEnter ""
+mkTodo :: Array Todo -> Signal HTML (Array Todo)
+mkTodo ts = loopW ts \ts' -> D.div' $ pure $ do
+  s <- textInputEnter "" "What do you want to do?" true
+  pure (cons {name: s, done: false} ts')
 
-todos :: Array String -> Signal HTML (Maybe (Array String))
-todos p = do
-  ts <- loopS p \p' -> mkTodo p' >>= (map catMaybes <<< traverse todo)
-  hold Nothing $ (pure (Just ts)) <$ D.button [P.onClick] [D.text "Persist"]
+todos :: Todos -> Signal HTML Todos
+todos s = loopS s \s' -> do
+  ts <- mkTodo s'.todos
+  ts' <- map catMaybes (traverse (todo s'.filter) ts)
+  filterButtons s' {todos = ts'}
 
-todo :: String -> Signal HTML (Maybe String)
-todo t = hold (Just t) $ D.div'
-    [ do _ <- D.span [P.onDoubleClick] [D.text t]
-         todo <$> D.span' [textInputEnter t]
+todo :: Filter -> Todo -> Signal HTML (Maybe Todo)
+todo p t = if runFilter p t
+  then hold (Just t) $ D.div'
+    [ todo p <<< (\b -> t {done = b}) <$> checkbox t.done
+    , do _ <- D.span [mark t.done, P.onDoubleClick] [D.text t.name]
+         todo p <<< (\s -> t {name = s}) <$> D.span' [textInputEnter t.name "" false]
     , defer (\_ -> always Nothing) <$ D.button [P.onClick] [D.text "Delete"]
     ]
+  else always (Just t)
+  where
+    runFilter All _ = true
+    runFilter Active t' = not t'.done
+    runFilter Completed t' = t'.done
+    checkbox b = not b <$ D.input [P._type "checkbox", P.checked b, P.onChange]
+    mark done = if done
+      then P.style {textDecoration: "line-through"}
+      else P.style {}
+
+filterButtons :: Todos -> Signal HTML Todos
+filterButtons s = hold s $ D.div' (mkFilter <$> filters)
+  where
+    mkFilter f = D.button [select f, defer (\_ -> filterButtons (s {filter = f})) <$ P.onClick] [D.text (show f)]
+    filters = [All, Active, Completed]
+    select f = if s.filter == f
+      then P.style {border:"2px solid lightgray"}
+      else P.style {}
