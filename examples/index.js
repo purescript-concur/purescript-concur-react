f79e6792feb6b2f56647fd921fe80c99e68b5633
Anupam Jain <ajnsit@gmail.com>
2018-09-10 01:07:08 +0530

    Move to using parcel

---
+++ examples/index.js
@@ -0,0 +1,6 @@
+import Main from "../output/Main";
+// AJ: TODO: Implement service workers
+// import registerServiceWorker from './registerServiceWorker';
+
+Main.main();
+// registerServiceWorker();
