56ff3438330f9e6760005a2759d6add5b56c3a79
Anupam Jain <ajnsit@gmail.com>
2019-04-16 11:41:44 +0530

    Check in code for bug test (not exposed as an example program)

---
+++ examples/Test/Bug.purs
@@ -0,0 +1,30 @@
+module Test.Bud where
+
+import Prelude
+
+import Concur.Core (Widget)
+import Concur.React (HTML)
+import Concur.React.DOM as D
+import Concur.React.Props as P
+import Effect.Class (liftEffect)
+import Effect.Console (log)
+
+effect :: Widget HTML Unit
+effect = do
+  void $ D.button [P.onClick] [D.text "Button"]
+  liftEffect $ log "EFFECT"
+
+query :: Widget HTML Boolean
+query = go false
+  where
+    go st = do
+      event <- D.div []
+        [ effect
+        , void $ D.button [ P.onClick ] [ D.text "Query" ] -- if this goes first everything works
+        ]
+      pure st
+
+bugWidget :: ∀ a. Widget HTML a
+bugWidget = do
+  a <- query
+  D.text (show a)
