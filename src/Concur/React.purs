ed06698d06582cc0d64bd2d7c667b0a8964b447b
Adam Barber <barberadam995@gmail.com>
2021-05-11 23:50:37 +0800

    model widget using callbacks insted of Free

--- src/Concur/React.purs
+++ src/Concur/React.purs
@@ -1,12 +1,14 @@
+
 module Concur.React where
 
 import Prelude
 
-import Concur.Core.Discharge (discharge, dischargePartialEffect)
-import Concur.Core.Types (Widget)
+import Concur.Core.Types (Widget, runWidget)
 import Data.Either (Either(..))
-import Data.Tuple (Tuple(..))
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Effect.Ref as Ref
+import Effect.Ref (Ref)
 import Effect.Console (log)
 import React as R
 
@@ -20,25 +22,56 @@ type ComponentState
 mkComponentState :: HTML -> ComponentState
 mkComponentState v = { view: v }
 
-componentClassWithMount :: forall a. Effect Unit -> Widget HTML a -> R.ReactClass {}
-componentClassWithMount onMount winit = R.component "Concur" component
+componentClassWithMount ::
+  forall a.
+  Effect Unit ->
+  Widget HTML a ->
+  (R.ReactElement -> Effect Unit) ->
+  Effect Unit
+componentClassWithMount onMount w renderer = do
+  thisRef <- Ref.new Nothing
+  void $ runWidget w \res -> do
+    case res of
+      Left v -> do
+        mt <- Ref.read thisRef
+        case mt of
+          Just this -> void $ R.writeState this (mkComponentState v)
+          Nothing -> renderer $ element onMount v thisRef
+      Right _ -> log "Application exited"
+
+render ::
+  ComponentState ->
+  R.ReactElement
+render st = R.toElement st.view
+
+component ::
+  Effect Unit ->
+  HTML ->
+  Ref (Maybe (R.ReactThis {} ComponentState)) ->
+  R.ReactThis {} ComponentState ->
+  Effect { componentDidMount :: Effect Unit
+  , render :: Effect R.ReactElement
+  , state :: ComponentState  }
+component onMount vinit ref this = do
+  pure { state: mkComponentState vinit
+       , render: render <$> R.getState this
+       , componentDidMount: onMount *> handler this vinit ref
+       }
   where
-    component this = do
-      Tuple winit' v <- dischargePartialEffect winit
-      pure { state: mkComponentState v
-           , render: render <$> R.getState this
-           , componentDidMount: onMount *> handler this (Right winit')
-           }
-    handler this (Right r) = do
-      v <- discharge (handler this) r
-      void $ R.writeState this (mkComponentState v)
-    handler _ (Left err) = do
-      log ("FAILED! " <> show err)
-      pure unit
-    render st = R.toElement st.view
+    handler t v r = do
+      Ref.write (Just t) r
+      void $ R.writeState t (mkComponentState v)
 
-componentClass :: forall a. Widget HTML a -> R.ReactClass {}
-componentClass = componentClassWithMount mempty
+element ::
+  Effect Unit ->
+  HTML ->
+  Ref (Maybe (R.ReactThis {} ComponentState)) ->
+  R.ReactElement
+element onMount v ref = R.createLeafElement (R.component "Concur" (component onMount v ref)) {}
 
-renderComponent :: forall a. Widget HTML a -> R.ReactElement
-renderComponent init = R.createLeafElement (componentClass init) {}
+renderComponent ::
+  forall a.
+  Widget HTML a ->
+  (R.ReactElement -> Effect Unit) ->
+  Effect Unit
+renderComponent winit renderer = componentClassWithMount mempty winit renderer
