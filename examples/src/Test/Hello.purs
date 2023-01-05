cd7d89b1b3ce0baf34cfb5f2a41d4a912df8cff8
Adam Barber <barberadam995@gmail.com>
2023-01-05 22:09:08 +0800

    restore hello example now that we have MonadRec

--- examples/src/Test/Hello.purs
+++ examples/src/Test/Hello.purs
@@ -6,6 +6,7 @@ import Concur.Core (Widget)
 import Concur.React (HTML)
 import Concur.React.DOM as D
 import Concur.React.Props as P
+import Control.Monad.Rec.Class (forever)
 import Control.Monad.State.Class (get, put)
 import Control.Monad.State.Trans (StateT, runStateT)
 
@@ -15,11 +16,10 @@ helloWidget = do
   D.text "Actually this will never be reached, but the compiler is not smart enough to deduce that"
 
 helloWidgetS :: forall a. StateT Int (Widget HTML) a
-helloWidgetS = do
+helloWidgetS = forever do
   count <- get
   void $ D.div' [ D.button [P.onClick] [D.text ("For the " <> show count <> " time, hello sailor!")] ]
   put (count + 1)
-  helloWidgetS
 
 -- Widgety Widget
 data View = View Int HTML
